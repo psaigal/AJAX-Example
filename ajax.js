8b57dacd30391a94f33317ba40becd89bfc3f167
psaigal <psaigal48@gmail.com>
2017-08-18 16:14:40 -0700

    Add AJAX example using vanilla JavaScript

--- ajax.js
+++ ajax.js
@@ -1,19 +1,31 @@
-document.getElementById("AJAX-button").onclick = function() {myFunction()};
+document.addEventListener("DOMContentLoaded", function(){
 
-var requestObject = new XMLHttpRequest();
+	var responseElement = document.getElementById("response");
+	var error = document.getElementsByClassName("error")[0];
 
-requestObject.onreadystatechange = function() {
-  if (requestObject.readyState === 4) {
-    if (requestObject.status === 200 || requestObject.status === 304) {
-      console.log(requestObject.statusText);
-      console.log(requestObject.responseText)
-    }
-  }
-}
+	var requestObject = new XMLHttpRequest();
 
-requestObject.open("POST", http:, true);
+	requestObject.onreadystatechange = function() { 
+		if(requestObject.readyState === 4) { 
+			if(requestObject.status === 200) {
+				var response = JSON.parse(requestObject.responseText);
+				for (var i = 0; i <= 10; i++) {
+					responseElement.innerHTML += 
+						"<ul>" + 
+							"<li>Company Name: " + response.networks[i].company[0] + "</li>" +
+							"<li>City: " + response.networks[i].location.city + "</li>" +
+							"<li>Country: " + response.networks[i].location.country + "</li>" +
+						"</ul><br>"
+				}
+			}
+			else if(requestObject.status !== 200) {
+				error.style.display = "block";
+				error.innerHTML = "Sorry there was an error fetching the data";
+			}
+		}
 
-requestObject.send();
+	}
 
+	requestObject.open("GET", "http://api.citybik.es/v2/networks"); 
+	requestObject.send(); 
 
-  document.getElementById("demo").onclick = function(){}
